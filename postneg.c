7a073d7bd175e74e750e72743dc41b6960a3bdab
ManishaPriyadharshiniM <manisha96priya@gmail.com>
2016-07-08 10:47:36 +0530

    finish

---
+++ postneg.c
@@ -0,0 +1,19 @@
+# postneg
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter the number");
+scanf("%d",a);
+if(a<0)
+{
+if(a==0)
+printf("the  no is zero");
+else
+printf("the no is negative");
+}
+else
+printf("the no is positive");
+}
+return 0;
+}
